b993cee92da415ee8896a2753b649c41dd066c3f
Ice-CubeXQ <dealplug72@gmail.com>
2024-10-22 22:39:02 +0300

    initial commit

---
+++ app/src/components/editor/editor.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Editor extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pageList: [],
+      newPageName: "",
+    };
+    this.createNewPage = this.createNewPage.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadPageList();
+  }
+
+  loadPageList() {
+    axios.get("./api").then((res) => this.setState({ pageList: res.data }));
+  }
+
+  createNewPage() {
+    axios
+      .post("./api/createNewPage.php", { name: this.state.newPageName })
+      .then(this.loadPageList())
+      .catch(() => alert("Страница уже существует"));
+  }
+
+  deletePage(page) {
+    axios
+      .post("./api/deletePage.php", { name: page })
+      .then(this.loadPageList())
+      .catch(() => alert("Страницы не существует"));
+  }
+
+  render() {
+    // const { pageList } = this.state;
+    // const pages = pageList.map((page, i) => {
+    //   return (
+    //     <h1 key={i}>
+    //       {page}
+    //       <a onClick={() => this.deletePage(page)} href="#">
+    //         (x)
+    //       </a>
+    //     </h1>
+    //   );
+    // });
+    return (
+      <iframe src="../index.html" frameBorder={0}></iframe>
+      // <>
+      //   <input
+      //     onChange={(e) => {
+      //       this.setState({ newPageName: e.target.value });
+      //     }}
+      //     type="text"
+      //   ></input>
+      //   <button onClick={this.createNewPage}>Создать страницу</button>
+      //   {pages}
+      // </>
+    );
+  }
+}
