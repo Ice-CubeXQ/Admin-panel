58c6f84573dfa48bfdff73968c820b92197ca291
Ice-CubeXQ <dealplug72@gmail.com>
2024-10-24 20:23:33 +0300

    Added UI panel for admin page

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,6 +1,10 @@
 import "../../helpers/iframeLoader.js";
 import React, { Component } from "react";
 import axios from "axios";
+import DOMHelper from "../../helpers/dom-helper.js";
+import EditorText from "../editor-text";
+import UIkit from "uikit";
+import Spinner from "../spinner/spinner.js";
 
 export default class Editor extends Component {
   constructor() {
@@ -11,8 +15,11 @@ export default class Editor extends Component {
     this.state = {
       pageList: [],
       newPageName: "",
+      loading: true,
     };
     this.createNewPage = this.createNewPage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
+    this.isLoaded = this.isLoaded.bind(this);
   }
 
   componentDidMount() {
@@ -21,85 +28,58 @@ export default class Editor extends Component {
 
   init(page) {
     this.iframe = document.querySelector("iframe");
-    this.open(page);
+    this.open(page, this.isLoaded);
     this.loadPageList();
   }
 
-  open(page) {
+  open(page, cb) {
     this.currentPage = page;
 
     axios
       .get(`../${page}?rnd=${Math.random()}`)
-      .then((res) => this.parseStrtoDOM(res.data))
-      .then(this.wrapTextNodes)
+      .then((res) => DOMHelper.parseStrtoDOM(res.data))
+      .then(DOMHelper.wrapTextNodes)
       .then((dom) => {
         this.virtualDom = dom;
         return dom;
       })
-      .then(this.serializeDOMToString)
+      .then(DOMHelper.serializeDOMToString)
       .then((html) => axios.post("./api/saveTempPage.php", { html }))
       .then(() => this.iframe.load("../temp.html"))
-      .then(() => this.enableEditing());
+      .then(() => this.enableEditing())
+      .then(() => this.injectStyles())
+      .then(cb);
   }
-  save() {
+  save(onSuccess, onError) {
+    this.isLoading();
     const newDom = this.virtualDom.cloneNode(this.virtualDom);
-    this.unwrapTextNodes(newDom);
-    const html = this.serializeDOMToString(newDom);
-    axios.post("./api/savePage.php", { pageName: this.currentPage, html });
+    DOMHelper.unwrapTextNodes(newDom);
+    const html = DOMHelper.serializeDOMToString(newDom);
+    axios.post("./api/savePage.php", { pageName: this.currentPage, html }).then(onSuccess).catch(onError).finally(this.isLoaded);
   }
 
   enableEditing() {
     this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach((element) => {
-      element.contentEditable = "true";
-      element.addEventListener("input", () => {
-        this.onTextEdit(element);
-      });
+      const id = element.getAttribute("nodeid");
+      const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`);
+
+      new EditorText(element, virtualElement);
     });
   }
 
-  onTextEdit(element) {
-    const id = element.getAttribute("nodeid");
-    this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML = element.innerHTML;
-  }
-
-  parseStrtoDOM(str) {
-    const parser = new DOMParser();
-    return parser.parseFromString(str, "text/html");
-  }
-  wrapTextNodes(dom) {
-    const body = dom.body;
-    let textNodes = [];
-
-    function recursy(element) {
-      element.childNodes.forEach((node) => {
-        if (node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
-          textNodes.push(node);
-        } else {
-          recursy(node);
-        }
-      });
+  injectStyles() {
+    const style = this.iframe.contentDocument.createElement("style");
+    style.innerHTML = `
+    text-editor:hover{
+      outline: 3px solid orange;
+      outline-offset: 8px;
     }
-
-    recursy(body);
-
-    textNodes.forEach((node, i) => {
-      const wrapper = dom.createElement("text-editor");
-      node.parentNode.replaceChild(wrapper, node);
-      wrapper.appendChild(node);
-      wrapper.setAttribute("nodeid", i);
-    });
-
-    return dom;
-  }
-  unwrapTextNodes(dom) {
-    dom.body.querySelectorAll("text-editor").forEach((element) => {
-      element.parentNode.replaceChild(element.firstChild, element);
-    });
-  }
-
-  serializeDOMToString(dom) {
-    const serializer = new XMLSerializer();
-    return serializer.serializeToString(dom);
+      text-editor:focus{
+      outline: 3px solid red;
+      outline-offset: 8px;
+    }
+    `;
+    this.iframe.contentDocument.head.appendChild(style);
   }
 
   loadPageList() {
@@ -120,34 +100,60 @@ export default class Editor extends Component {
       .catch(() => alert("Страницы не существует"));
   }
 
+  isLoading() {
+    this.setState({ loading: true });
+  }
+  isLoaded() {
+    this.setState({ loading: false });
+  }
+
   render() {
-    // const { pageList } = this.state;
-    // const pages = pageList.map((page, i) => {
-    //   return (
-    //     <h1 key={i}>
-    //       {page}
-    //       <a onClick={() => this.deletePage(page)} href="#">
-    //         (x)
-    //       </a>
-    //     </h1>
-    //   );
-    // });
+    const { loading } = this.state;
+    const modal = true;
+
+    let spinner;
+    loading ? (spinner = <Spinner active />) : (spinner = <Spinner />);
+
     return (
       <>
-        <button onClick={() => this.save()}>click</button>
         <iframe src={this.currentPage}></iframe>
-      </>
 
-      // <>
-      //   <input
-      //     onChange={(e) => {
-      //       this.setState({ newPageName: e.target.value });
-      //     }}
-      //     type="text"
-      //   ></input>
-      //   <button onClick={this.createNewPage}>Создать страницу</button>
-      //   {pages}
-      // </>
+        {spinner}
+
+        <div id="panel" className="panel">
+          <button uk-toggle="target: #modal-save" className="uk-button uk-button-primary">
+            Опубликовать
+          </button>
+        </div>
+
+        <div id="modal-save" uk-modal={modal.toString()} container="#panel">
+          <div className="uk-modal-dialog uk-modal-body">
+            <h2 className="uk-modal-title">Сохранение</h2>
+            <p>Вы действительно хотите сохранить изменения?</p>
+            <p className="uk-text-right">
+              <button className="uk-button uk-button-default uk-modal-close" type="button">
+                Отменить
+              </button>
+              <button
+                className="uk-button uk-button-primary uk-modal-close"
+                type="button"
+                onClick={() =>
+                  this.save(
+                    () => {
+                      UIkit.notification({ message: "Успешно сохранено", status: "success" });
+                    },
+                    () => {
+                      UIkit.notification({ message: "Ошибка сохранения", status: "danger" });
+                    }
+                  )
+                }
+              >
+                Опубликовать
+              </button>
+            </p>
+          </div>
+        </div>
+      </>
     );
   }
 }
