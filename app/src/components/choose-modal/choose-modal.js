3a764c67a9f628a0979e442c78ab03cb4a7b4bff
Ice-CubeXQ <dealplug72@gmail.com>
2024-10-29 21:06:48 +0300

    added page restore functionality

--- app/src/components/choose-modal/choose-modal.js
+++ app/src/components/choose-modal/choose-modal.js
@@ -1,21 +1,37 @@
 import React from "react";
 
 const ChooseModal = ({ modal, target, data, redirect }) => {
-  const pageList = data.map((item) => {
-    return (
-      <li key={item}>
-        <a onClick={(e) => redirect(e, item)} className="uk-link-muted uk-modal-close" href="#">
-          {item}
-        </a>
-      </li>
-    );
+  const list = data.map((item) => {
+    if (item.time) {
+      return (
+        <li key={item.file}>
+          <a onClick={(e) => redirect(e, item.file)} className="uk-link-muted uk-modal-close" href="#">
+            Резерваня копия от {item.time}
+          </a>
+        </li>
+      );
+    } else {
+      return (
+        <li key={item}>
+          <a onClick={(e) => redirect(e, item)} className="uk-link-muted uk-modal-close" href="#">
+            {item}
+          </a>
+        </li>
+      );
+    }
   });
+
+  let msg;
+  if (data.length < 1) {
+    msg = <div>Резервные копии не найдены</div>;
+  }
 
   return (
     <div id={target} uk-modal={modal.toString()} container="#root">
       <div className="uk-modal-dialog uk-modal-body">
         <h2 className="uk-modal-title">Открыть</h2>
-        <ul className="uk-list uk-list-divider">{pageList}</ul>
+        {msg}
+        <ul className="uk-list uk-list-divider">{list}</ul>
 
         <p className="uk-text-right">
           <button className="uk-button uk-button-default uk-modal-close" type="button">
--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -7,6 +7,7 @@ import UIkit from "uikit";
 import Spinner from "../spinner/spinner.js";
 import ConfirmModal from "../confirm-modal/confirm-modal.js";
 import ChooseModal from "../choose-modal/choose-modal.js";
+import Panel from "../panel/panel.js";
 
 export default class Editor extends Component {
   constructor() {
@@ -16,14 +17,16 @@ export default class Editor extends Component {
 
     this.state = {
       pageList: [],
+      backupsList: [],
       newPageName: "",
       loading: true,
     };
-    this.createNewPage = this.createNewPage.bind(this);
+
     this.isLoading = this.isLoading.bind(this);
     this.isLoaded = this.isLoaded.bind(this);
     this.save = this.save.bind(this);
     this.init = this.init.bind(this);
+    this.restoreBackup = this.restoreBackup.bind(this);
   }
 
   componentDidMount() {
@@ -38,6 +41,7 @@ export default class Editor extends Component {
     this.iframe = document.querySelector("iframe");
     this.open(page, this.isLoaded);
     this.loadPageList();
+    this.loadBackupsList();
   }
 
   open(page, cb) {
@@ -58,13 +62,17 @@ export default class Editor extends Component {
       .then(() => this.enableEditing())
       .then(() => this.injectStyles())
       .then(cb);
+
+    this.loadBackupsList();
   }
-  save(onSuccess, onError) {
+  async save(onSuccess, onError) {
     this.isLoading();
     const newDom = this.virtualDom.cloneNode(this.virtualDom);
     DOMHelper.unwrapTextNodes(newDom);
     const html = DOMHelper.serializeDOMToString(newDom);
-    axios.post("./api/savePage.php", { pageName: this.currentPage, html }).then(onSuccess).catch(onError).finally(this.isLoaded);
+    await axios.post("./api/savePage.php", { pageName: this.currentPage, html }).then(onSuccess).catch(onError).finally(this.isLoaded);
+
+    this.loadBackupsList();
   }
 
   enableEditing() {
@@ -95,18 +103,29 @@ export default class Editor extends Component {
     axios.get("./api/pageList.php").then((res) => this.setState({ pageList: res.data }));
   }
 
-  createNewPage() {
-    axios
-      .post("./api/createNewPage.php", { name: this.state.newPageName })
-      .then(this.loadPageList())
-      .catch(() => alert("Страница уже существует"));
+  loadBackupsList() {
+    axios.get("./backups/backups.json").then((res) =>
+      this.setState({
+        backupsList: res.data.filter((backup) => {
+          return backup.page === this.currentPage;
+        }),
+      })
+    );
   }
 
-  deletePage(page) {
-    axios
-      .post("./api/deletePage.php", { name: page })
-      .then(this.loadPageList())
-      .catch(() => alert("Страницы не существует"));
+  restoreBackup(e, backup) {
+    if (e) {
+      e.preventDefault();
+    }
+    UIkit.modal
+      .confirm("Вы действительно хотите восстановить эту страницу из этой резервной копии? Все несохраненные данные будут потеряны!", { labels: { ok: "Восстановить", cancel: "Отмена" } })
+      .then(() => {
+        this.isLoading();
+        return axios.post("./api/restoreBackup.php", { page: this.currentPage, file: backup });
+      })
+      .then(() => {
+        this.open(this.currentPage, this.isLoaded);
+      });
   }
 
   isLoading() {
@@ -117,7 +136,7 @@ export default class Editor extends Component {
   }
 
   render() {
-    const { loading, pageList } = this.state;
+    const { loading, pageList, backupsList } = this.state;
     const modal = true;
 
     let spinner;
@@ -125,21 +144,15 @@ export default class Editor extends Component {
 
     return (
       <>
-        <iframe src={this.currentPage}></iframe>
+        <iframe src=""></iframe>
 
         {spinner}
 
-        <div className="panel">
-          <button uk-toggle="target: #modal-open" className="uk-button uk-button-primary uk-margin-small-right">
-            Открыть
-          </button>
-          <button uk-toggle="target: #modal-save" className="uk-button uk-button-primary">
-            Опубликовать
-          </button>
-        </div>
+        <Panel />
 
         <ConfirmModal modal={modal} target={"modal-save"} method={this.save} />
         <ChooseModal modal={modal} target={"modal-open"} data={pageList} redirect={this.init} />
+        <ChooseModal modal={modal} target={"modal-backup"} data={backupsList} redirect={this.restoreBackup} />
       </>
     );
   }
